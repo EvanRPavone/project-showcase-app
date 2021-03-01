cc7cdbf5df80a514c4a1e8e45850f8dd8e8204ec
EvanRPavone <evanrpavone@gmail.com>
2021-03-01 11:35:38 -0500

    Replaced "Add Project" with "+"

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <div>
             <h1>Project Showcase</h1>
             <p>Hello, welcome to Project Showcase! To get started, login or signup in the top right corner. Once you have logged in, go to the Projects tab to see all the projects</p>
-            <p>You can add a project by clicking Add Project above the Project Cards, then fill out the information fully.</p>
+            <p>You can add a project by clicking + above the Project Cards, then fill out the information fully.</p>
         </div>
     )
 }
